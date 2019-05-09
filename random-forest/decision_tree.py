266f8ed97bd73b0aa6e3249c6e3aeaf498df1847
Francisco Bento <franciscobdsjunior@gmail.com>
2019-05-09 17:55:08 -0300

    Fix decision tree to accept same node in differents paths

--- random-forest/decision_tree.py
+++ random-forest/decision_tree.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 from anytree import Node, RenderTree, AnyNode
+from anytree.dotexport import DotExporter
 
 NODE_TYPE_DECISION = 'D'
 NODE_TYPE_LABEL = 'L'
@@ -14,16 +15,24 @@ def generate_tree(d, attributes, parent=None, parent_value=None):
     print('DATASET')
     print(d)
     print('-'*50)
+
+    original_entropy = 0.0
+    y_field = d.columns[len(d.columns) - 1]
+    original_classes = d[y_field].unique()
+    attributes = d.columns[:-1].tolist()
+
     print('COLUMNS')
     print(attributes)
     print('-'*50)
     print()
-    original_entropy = 0.0
-    y_field = d.columns[len(d.columns) - 1]
-    original_classes = d[y_field].unique()
 
     if len(original_classes) == 1:
-        return AnyNode(parent, type=NODE_TYPE_LABEL, label=original_classes[0], value=parent_value)
+        return AnyNode(parent, type=NODE_TYPE_LABEL, label=original_classes[0], value=parent_value,
+                       name=parent_value + '\n= ' + original_classes[0])
+    elif not attributes:
+        most_frequent_label = d[y_field].max()
+        return AnyNode(parent, type=NODE_TYPE_LABEL, label=most_frequent_label, value=parent_value,
+                       name=parent_value + '\n= ' + most_frequent_label)
 
     for c in original_classes:
         d_c = d[d.apply(lambda x: x[y_field] == c, axis=1)]
@@ -69,9 +78,10 @@ def generate_tree(d, attributes, parent=None, parent_value=None):
 
     decision = AnyNode(parent,
                        type=NODE_TYPE_DECISION,
+                       name=str(parent_value) + '\n' + chosen_field + '?',
                        field=chosen_field, value=parent_value)
 
-    attributes.remove(chosen_field)
+    # attributes.remove(chosen_field)
 
     for v in d[chosen_field].unique():
         print(chosen_field, v)
@@ -87,7 +97,10 @@ def create_decision_tree(filename, separator=';'):
     root = generate_tree(dataset, dataset.columns[:-1].tolist())
 
     print(RenderTree(root))
+    # DotExporter(root).to_picture('{}.png'.format(filename.replace('.csv', '')))
 
 
 if __name__ == "__main__":
     create_decision_tree('datasets/benchmark.csv')
+    # create_decision_tree('datasets/test.csv', ',')
+    # create_decision_tree('datasets/wrong.csv', ',')
