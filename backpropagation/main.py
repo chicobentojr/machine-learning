60b32e9e16e39628026076cd21671113ee644a27
Flavia <31422365+FlaviaAP@users.noreply.github.com>
2019-06-19 19:06:59 -0300

    fix main backprop command for Method of Momment

--- backpropagation/main.py
+++ backpropagation/main.py
@@ -13,14 +13,15 @@ cli = click.Group()
 @click.argument('network_filename')
 @click.argument('initial_weights_filename')
 @click.argument('data_set_filename')
-@click.option('--alpha', '-a', default=0.5, help='Weights Update Rate, is used to smooth the gradient')
+@click.option('--alpha', '-a', default=0.1, help='Weights Update Rate, is used to smooth the gradient')
+@click.option('--beta', '-b', default=0.9, help='Relevance of recent average direction (method of moment)')
 @click.option('--max_iterations', '-it', default=100, help='Maximum number of iterations.')
 def backprop(network_filename, initial_weights_filename, data_set_filename,
-             max_iterations, alpha):
+             max_iterations, alpha, beta):
 
     bp.logger = logger
     bp.backpropagation(network_filename, initial_weights_filename, data_set_filename,
-                    max_iterations, alpha)
+                    max_iterations, alpha, beta)
 
 
 
