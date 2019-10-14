6d9b1ea61cd6a0e3b69ea1799398988fc1c7d6c7
Francisco Bento <franciscobdsjunior@gmail.com>
2019-10-14 13:00:44 -0300

    Add DynaQ agent

--- reinforcement-learning/4-task/reinforcement/dynaQAgents.py
+++ reinforcement-learning/4-task/reinforcement/dynaQAgents.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -17,7 +17,10 @@
 from game import *
 from learningAgents import ReinforcementAgent
 
-import random,util,math
+import random
+import util
+import math
+
 
 class DynaQAgent(ReinforcementAgent):
     """
@@ -40,11 +43,15 @@ class DynaQAgent(ReinforcementAgent):
         - self.getLegalActions(state)
           which returns legal actions for a state
     """
+
     def __init__(self, plan_steps=5, kappa=0, **args):
         "You can initialize Q-values here..."
         ReinforcementAgent.__init__(self, **args)
 
         "*** YOUR CODE HERE ***"
+        self.Q = util.Counter()
+        self.model = util.Counter()
+        self.plan_steps = plan_steps
 
     def getQValue(self, state, action):
         """
@@ -53,8 +60,7 @@ class DynaQAgent(ReinforcementAgent):
           or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        return self.Q[(state, action)]
 
     def computeValueFromQValues(self, state):
         """
@@ -64,7 +70,19 @@ class DynaQAgent(ReinforcementAgent):
           terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+
+        if not actions:
+            return 0.0
+
+        max_value = None
+
+        for action in actions:
+            q_value = self.getQValue(state, action)
+            if max_value is None or q_value > max_value:
+                max_value = q_value
+
+        return max_value
 
     def computeActionFromQValues(self, state):
         """
@@ -73,7 +91,20 @@ class DynaQAgent(ReinforcementAgent):
           you should return None.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        actions = self.getLegalActions(state)
+
+        if not actions:
+            return None
+
+        best_actions = []
+        best_action_value = self.getValue(state)
+
+        for action in actions:
+            q_value = self.getQValue(state, action)
+            if q_value == best_action_value:
+                best_actions.append(action)
+
+        return random.choice(best_actions)
 
     def getAction(self, state):
         """
@@ -90,7 +121,11 @@ class DynaQAgent(ReinforcementAgent):
         legalActions = self.getLegalActions(state)
         action = None
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if legalActions:
+            if util.flipCoin(self.epsilon):
+                action = random.choice(legalActions)
+            else:
+                action = self.computeActionFromQValues(state)
 
         return action
 
@@ -106,7 +141,30 @@ class DynaQAgent(ReinforcementAgent):
           NOTE2: insert your planning code here as well
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        q_value = self.getQValue(state, action)
+
+        next_action = self.computeActionFromQValues(nextState)
+        next_q_value = self.getQValue(nextState, next_action)
+
+        new_q_value = q_value + self.alpha * \
+            (reward + self.discount * next_q_value - q_value)
+
+        self.Q[(state, action)] = new_q_value  # Direct RL
+        self.model[(state, action)] = (reward, nextState)  # Model learning
+
+        # Planning
+        for i in range(self.plan_steps):
+            state, action = random.choice(self.model.keys())
+            reward, nextState = self.model[(state, action)]
+
+            q_value = self.getQValue(state, action)
+            next_action = self.computeActionFromQValues(nextState)
+            next_q_value = self.getQValue(nextState, next_action)
+
+            new_q_value = q_value + self.alpha * \
+                (reward + self.discount * next_q_value - q_value)
+
+            self.Q[(state, action)] = new_q_value
 
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
@@ -118,7 +176,7 @@ class DynaQAgent(ReinforcementAgent):
 class PacmanDynaQAgent(DynaQAgent):
     "Exactly the same as DynaAgent, but with different default parameters"
 
-    def __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
+    def __init__(self, epsilon=0.05, gamma=0.8, alpha=0.2, numTraining=0, **args):
         """
         These default parameters can be changed from the pacman.py command line.
         For example, to change the exploration rate, try:
@@ -143,5 +201,5 @@ class PacmanDynaQAgent(DynaQAgent):
         method.
         """
         action = DynaQAgent.getAction(self, state)
-        self.doAction(state,action)
+        self.doAction(state, action)
         return action
